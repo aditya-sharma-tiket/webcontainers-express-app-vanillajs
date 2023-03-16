4165d0734fd75ab49ed50a72e60f76b168faad42
Aditya Sharma <aditya.sharma@tiket.com>
2023-03-16 10:21:42 +0530

    testing node on browser

--- main.js
+++ main.js
@@ -1,113 +1,58 @@
-import './style.css';
-import { WebContainer } from '@webcontainer/api';
-import { files } from './files';
-import Bonjour from 'bonjour-service';
-import  {Client}  from 'node-ssdp';
+import { WebContainer } from "@webcontainer/api";
 
-
-/** @type {import('@webcontainer/api').WebContainer}  */
-let webcontainerInstance;
-
-window.addEventListener('load', async () => {
-  textareaEl.value = files['index.js'].file.contents;
-
-  textareaEl.addEventListener('input', (e) => {
-    writeIndexJS(e.currentTarget.value);
-  });
-  // Call only once
-  webcontainerInstance = await WebContainer.boot();
-  await webcontainerInstance.mount(files);
-
-  /**
-   * To confirm that it worked, 
-   * let's read the contents of package.json from WebContainer
-   * using the fs.readFile method provided by webcontainerInstance 
-   * and log it into the console: 
-  **/
-  // const packageJSON = await webcontainerInstance.fs.readFile('package.json', 'utf-8');
-  // console.log(packageJSON);
-  const exitCode = await installDependencies();
-  if (exitCode !== 0) {
-    throw new Error('Installation failed');
-  };
-  
-  startDevServer();
-});
-
-
-
-async function installDependencies() {
-  // Install dependencies
-  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
-
-  //read the output of this command.
-  // installProcess.output.pipeTo(new WritableStream({
-  //   write(data) {
-  //     console.log('help',data);
-  //   }
-  // }));
-
-
-  // Wait for install command to exit
-  return installProcess.exit;
-}
-async function bonjour(){
-  // var bonjour = require('bonjour')();
-  // var bonjour=new Bonjour();
-  // bonjour.find({ type: 'tms',protocol:'tcp' }, function (service) {
-  //   console.log('Found an HTTP server:', service)
-  // })
-  // var Client = require('node-ssdp').Client
-      var client =new Client();
-      client.on('response', function (headers, statusCode, rinfo) {
-        console.log('Got a response to an m-search.');
-      });client.search('urn:schemas-upnp-org:service:ContentDirectory:1');
-
-
-}
-
-async function startDevServer() {
-  // Run `npm run start` to start the Express app
-  await webcontainerInstance.spawn('npm', ['run', 'start']);
-
-// bonjour();
-  // Wait for `server-ready` event
-  webcontainerInstance.on('server-ready', (port, url) => {
-    iframeEl.src = url;
-  });
-}
-
-/** @param {string} content*/
-
-async function writeIndexJS(content) {
-  await webcontainerInstance.fs.writeFile('/index.js', content);
+const reportOutput = (output) => {
+  outputPannel.textContent += "\n" + output;
 };
 
+window.addEventListener("load", async () => {
+  reportOutput("Booting.....");
+  const wc = await WebContainer.boot();
+  reportOutput("Booting Complete");
 
+  const runCommand = async (command, args) => {
+    const process = await wc.spawn(command, args);
 
+    process.output.pipeTo(
+      new WritableStream({
+        write: (chunck) => {
+          reportOutput(`Process output : ${chunck}`);
+        },
+      })
+    );
+
+    if (await process.exit) {
+      reportOutput(`Process failed and exited with code ${process.exit}`);
+    }
+  };
+
+  // await runCommand('echo',['hello world']);
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const cmd = command.value.split(" ")[0];
+    const args = command.value.split(" ").slice(1);
+    await runCommand(cmd, args);
+  });
+});
 
 // Display
-document.querySelector('#app').innerHTML = `
-  <div class="container">
-    <div class="editor">
-      <textarea>I am a textarea</textarea>
-    </div>
-    <div class="preview">
-      <iframe src="loading.html"></iframe>
-    </div>
-  </div>
-`
-/** @type {HTMLIFrameElement | null} */
-const iframeEl = document.querySelector('iframe');
-
-/** @type {HTMLTextAreaElement | null} */
-const textareaEl = document.querySelector('textarea');
+document.querySelector("#app").innerHTML = `
+<form id="form">
+<label> Command 
+<input type='text' id='command'/>
+</label>
+<button>Run</button>
+</form>
+<pre>
+  <code id="outputPannel" style="display: flex;height: 400px;background-color: black;color: white;border-radius: 4px;padding: 8px;width: 80% ; overflow: auto; flex-direction: column-reverse ;"> </code>
+</pre>
+`;
 
 
+const form = document.getElementById("form");
 
-/*
-const instance = new Bonjour()
-instance.find({ type: 'tms' }, function (service) {
-  console.log('Found an http server:', service)
-});
-*/
+
+const outputPannel = document.getElementById("outputPannel");
+
+
+const command = document.getElementById("command");
